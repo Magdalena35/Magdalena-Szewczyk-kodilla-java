0127e44d2d9dfbc0edfadcd15cc8d6445cb9d832
Magdalena35 <mada.szewczyk@gmail.com>
2017-12-09 18:33:39 +0100

    Initial commit

---
+++ src/test/java/com/kodilla/kodillakurs/KodillaKursApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodilla.kodillakurs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodillaKursApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
