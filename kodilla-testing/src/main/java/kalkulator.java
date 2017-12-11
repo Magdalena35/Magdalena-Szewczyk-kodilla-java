405c20be420612c14e9693bfb011450939824130
Magdalena35 <mada.szewczyk@gmail.com>
2017-12-11 21:26:12 +0100

    Kalkulator2

---
+++ kodilla-testing/src/main/java/kalkulator.java
@@ -0,0 +1,15 @@
+public class kalkulator {
+
+    public static void main(String [] args){
+
+    int a =20;
+    int b=10;
+
+    int result = a-b;
+    int result2 = a+b;
+
+    System.out.println(result);
+    System.out.println(result2);
+}
+
+}
