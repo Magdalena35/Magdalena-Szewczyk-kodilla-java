5a38bc38add04f6235f6e2d50bacbc0c07380f54
Magdalena35 <mada.szewczyk@gmail.com>
2018-01-05 23:26:03 +0100

    Stream Immutable

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumComment.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.forum;
+
+public class ForumComment {
+    ForumPost forumPost;
+    String commentBody;
+    String author;
+
+    public ForumComment(ForumPost forumPost,String commentBody, String author){}
+
+}
