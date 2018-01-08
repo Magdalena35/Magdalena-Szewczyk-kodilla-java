75591ca1b9b9a0e825d258aebd06e48831fc6f3e
Magdalena35 <mada.szewczyk@gmail.com>
2018-01-08 22:07:28 +0100

    Forum

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
@@ -0,0 +1,21 @@
+package com.kodilla.stream.forumuser;
+
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.lang.*;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        Forum forum = new Forum();
+        forum.getList().stream
+                //.filter(ForumUser -> ForumUser.sex != 'M')
+                //.filter((ForumUser) -> ForumUser. >= (1998))
+                //.filter((ForumUser) -> ForumUser.PostNumber) =>(1)
+
+
+    }
+}
